cc1093a4588118a1fea9f18a93be7c43ea7b2e2f
huadyng <huydang882125@gmail.com>
2025-10-27 00:43:31 +0700

    changJS2

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,6 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="/login" element={<Login />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
